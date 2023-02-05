4fa51cd0c3594c74918220c24d75cbb04af3bed3
David Stevenson <drstevenson1958@yahoo.com>
2023-02-05 18:09:47 -0500

    Chapter 2 changes

---
+++ ch02/apostrophe.py
@@ -0,0 +1,8 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+message = 'One of Python\'s strengths is its diverse community.'
+print(message)
+
+message = "Albert Einstein once said, \"A person who never made a mistake never tried anything new.\""
+print(message)
---
+++ ch02/ch02.py
@@ -0,0 +1,65 @@
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+
+favorite_language = ' python  '
+favorite_language
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+
+Numbers
+
+Integers
+
+2 + 3
+3 - 2
+2 * 3
+3 / 2
+
+
+3 ** 2
+3 ** 3
+10 ** 6
+
+Floats
+
+0.1 + 0.1
+0.2 + 0.2
+2* 0.1
+2 * 0.2
+0.2 + 0.1
+3 * 0.1
+
+Integers and Floats
+
+4/2
+
+1 + 2.0
+2 * 3.0
+3.0 ** 2
+
+
+Undercores in Numbers
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+Multiple Assignment
+
+x, y, z = 0, 0, 0
+
+Constants
+
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
+
+Comments
+
+# Say hello to everyone.
+print("Hello Python people!")
+
+import this
+# The Zen of Python, by Tim Peters
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
